38bef69eb007222f7442ac6fd45db57cf36960f8
mariarubioing <maria.rubio.ing@gmail.com>
2026-06-12 21:39:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 const seal = document.getElementById("seal");
-const flap = document.querySelector(".flap");
-const paper = document.getElementById("paper");
 const envelope = document.getElementById("envelope");
 const invitation = document.getElementById("invitation");
-const music = document.getElementById("music");
 
 seal.addEventListener("click", () => {
-    flap.style.transform = "rotateX(-180deg)";
-    paper.style.opacity = "1";
+  envelope.classList.add("open");
 
-    setTimeout(() => {
-        envelope.style.display = "none";
-        invitation.classList.remove("hidden");
-        music.play();
-    }, 1500);
+  setTimeout(() => {
+    envelope.style.display = "none";
+    invitation.classList.remove("hidden");
+  }, 1200);
 });
+
 
